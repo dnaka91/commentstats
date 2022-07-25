1bb4abf529c629ce6b3fdd58d8653a4cf247aa45
Dominik Nakamura <dnaka91@gmail.com>
2022-07-25 12:50:38 +0900

    Remove SVG optimization

--- src/render.rs
+++ src/render.rs
@@ -5,14 +5,12 @@ use std::{
     path::PathBuf,
 };
 
-use anyhow::{anyhow, Context, Result};
+use anyhow::{Context, Result};
 use bincode::Options;
 use chrono::NaiveDate;
 use itertools::{Itertools, MinMaxResult};
 use plotters::prelude::*;
 use rayon::prelude::*;
-use svgcleaner::{cleaner, CleaningOptions, ParseOptions, StyleJoinMode, WriteOptions};
-use svgdom::{AttributesOrder, Indent, ListSeparator};
 use tokei::LanguageType;
 use zip::ZipArchive;
 use zstd::Decoder as ZstdDecoder;
@@ -82,9 +80,6 @@ pub fn run(mut filter: Vec<LanguageType>, input: PathBuf, size: (u32, u32)) -> R
     drop(chart);
     drop(root);
 
-    println!("optimizing...");
-
-    let buf = optimize(&buf)?;
     fs::write("stats.svg", &buf)?;
 
     println!("done");
@@ -155,82 +150,4 @@ fn minmax_value<T: Copy>(mmr: MinMaxResult<T>) -> Option<(T, T)> {
         MinMaxResult::OneElement(v) => Some((v, v)),
         MinMaxResult::MinMax(min, max) => Some((min, max)),
     }
-}
-
-fn optimize(data: &str) -> Result<Vec<u8>> {
-    let parse_options = ParseOptions {
-        parse_comments: true,
-        parse_declarations: true,
-        parse_unknown_elements: true,
-        parse_unknown_attributes: true,
-        parse_px_unit: false,
-        skip_unresolved_classes: true,
-        skip_invalid_attributes: false,
-        skip_invalid_css: false,
-        skip_paint_fallback: false,
-    };
-    let cleaning_options = CleaningOptions {
-        remove_unused_defs: true,
-        convert_shapes: true,
-        remove_title: true,
-        remove_desc: true,
-        remove_metadata: true,
-        remove_dupl_linear_gradients: true,
-        remove_dupl_radial_gradients: true,
-        remove_dupl_fe_gaussian_blur: true,
-        ungroup_groups: true,
-        ungroup_defs: true,
-        group_by_style: true,
-        merge_gradients: true,
-        regroup_gradient_stops: true,
-        remove_invalid_stops: true,
-        remove_invisible_elements: true,
-        resolve_use: true,
-        remove_version: true,
-        remove_unreferenced_ids: true,
-        trim_ids: true,
-        remove_text_attributes: true,
-        remove_unused_coordinates: true,
-        remove_default_attributes: true,
-        remove_xmlns_xlink_attribute: true,
-        remove_needless_attributes: true,
-        remove_gradient_attributes: false,
-        join_style_attributes: StyleJoinMode::Some,
-        apply_transform_to_gradients: true,
-        apply_transform_to_shapes: true,
-        paths_to_relative: true,
-        remove_unused_segments: true,
-        convert_segments: true,
-        apply_transform_to_paths: false,
-        coordinates_precision: 6,
-        properties_precision: 6,
-        paths_coordinates_precision: 8,
-        transforms_precision: 8,
-    };
-    let write_options = WriteOptions {
-        indent: Indent::None,
-        attributes_indent: Indent::None,
-        use_single_quote: false,
-        trim_hex_colors: true,
-        write_hidden_attributes: false,
-        remove_leading_zero: true,
-        use_compact_path_notation: true,
-        join_arc_to_flags: false,
-        remove_duplicated_path_commands: true,
-        use_implicit_lineto_commands: true,
-        simplify_transform_matrices: true,
-        list_separator: ListSeparator::Space,
-        attributes_order: AttributesOrder::Alphabetical,
-    };
-
-    let mut document = cleaner::parse_data(data, &parse_options)
-        .map_err(|e| anyhow!("failed parsing SVG document: {}", e))?;
-
-    cleaner::clean_doc(&mut document, &cleaning_options, &write_options)
-        .map_err(|e| anyhow!("failed cleaning up SVG document: {}", e))?;
-
-    let mut buf = Vec::new();
-    cleaner::write_buffer(&document, &write_options, &mut buf);
-
-    Ok(buf)
 }
